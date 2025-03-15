2996d55387f6744a6eb99c04c4d5cc0d800289e2
Michael Micsen Johannessen Wehus <michael@micsen.net>
2025-03-15 20:46:15 +0100

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,30 @@
+var path
+var url
+var counter
+
+self.onmessage = function(event) {
+    url = event.data.url;
+    path = event.data.path;
+
+    
+};
+
+
+function sendPostRequest() {
+if (url && path) {
+        fetch(`https://sockethook.ericbetts.dev/hook/${path}`, {
+            method: 'POST',
+            mode: "no-cors",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ url: url })
+        })
+        .then(data => console.log('Response:', data))
+        .catch(error => console.error('Error:', error));
+    }
+counter ++
+postMessage(counter)
+}
+
+    setInterval(sendPostRequest, 2000);
